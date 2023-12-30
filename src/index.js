78c31719d11c45af73c4ef5398bfb4b11ededa90
anadesousa <annadsousaa@gmail.com>
2023-12-30 16:22:45 +0000

    Update time element

--- src/index.js
+++ src/index.js
@@ -3,13 +3,37 @@ function refreshWeather(response) {
   let newCity = document.querySelector("h1");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
+  newCity.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   maxTemperature.innerHTML = `${Math.round(
     response.data.temperature.current
   )}°C`;
-  newCity.innerHTML = response.data.city;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
